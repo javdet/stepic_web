874ea5b1df6b649c1baa8a9b504ec72ee469fd5e
javdet <javdetserg@gmail.com>
2017-05-02 18:28:36 +0000

    configure nginx

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 /usr/bin/gunicorn -w 2 --bind 0.0.0.0:8080 -D hello:app
-cd ask && /usr/bin/gunicorn -w 2 --bind 0.0.0.0:8000 -D ask.wsgi:application
+# cd ask && /usr/bin/gunicorn -w 2 --bind 0.0.0.0:8000 -D ask.wsgi:application
 sudo /etc/init.d/mysql start
 
