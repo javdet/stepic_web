0f5674c98b30dd23b198e441bb0b993e7dfeb44e
javdet <javdetserg@gmail.com>
2017-05-04 22:01:19 +0500

    Правки приложения

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 def app(environ, start_response):
-    d = environ['QUERY_STRING'].split('=')
+    d = dict(x.split("=") for x in environ['QUERY_STRING'].split("&"))
     status='200 OK'
     headers = [
         ('Content-type', 'text/plain')
     ]
     start_response(status, headers)
-    return [d]
+    body = ""
+    for k in d.keys():
+        body += k + b"=" + d[k] + b"\n"
+    return [body]
