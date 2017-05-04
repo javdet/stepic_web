6ebda471223a98ebaf3949071a554dc469600913
javdet <javdetserg@gmail.com>
2017-05-04 23:39:41 +0500

    Добил hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 def app(environ, start_response):
-    d = dict(x.split("=") for x in environ['QUERY_STRING'].split("&"))
+    d = environ['QUERY_STRING'].split("&")
     status='200 OK'
     headers = [
         ('Content-type', 'text/plain')
     ]
     start_response(status, headers)
     body = ""
-    for k in d.keys():
-        body += k + b"=" + d[k] + b"\n"
+    for k in d:
+        body += k + b"\n"
     return [body]
