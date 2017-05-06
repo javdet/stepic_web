a123907b44f4afadc7b6ab704faebf1045497902
javdet <javdetserg@gmail.com>
2017-05-06 12:13:31 +0500

    Поправил урл

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,7 +19,7 @@ from qa import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-#    url(r'/', views.test, name='index'),
+    url(r'^$', views.test, name='index'),
     url(r'^login/', views.test, name='login'),
     url(r'^signup/', views.test, name='signup'),
     url(r'^question/\d+/', views.test, name='question'),
